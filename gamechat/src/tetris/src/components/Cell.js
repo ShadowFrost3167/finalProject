7bc240a759c10dd14f8f916b15451b179dfba31f
ShadowFrost3167 <olivertwist3167@gmail.com>
2024-06-06 14:25:59 +0300

    deleteTetrisOrganizedFiles

--- gamechat/src/tetris/src/components/Cell.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { StyledCell } from './styles/StyleCell';
-import {TETROMINOS} from '../tetrominos';
-const Cell = ({type}) => (
-    <StyledCell type = { type } color = {TETROMINOS[ type ].color}/>
-);
-
-export default Cell;
--- gamechat/src/tetris/src/gameHelper.js
+++
@@ -1,9 +0,0 @@
-export const STAGE_WIDTH = 12;
-export const STAGE_HEIGHT = 20;
-
-export const createStage = () => 
-    //array represents grid
-    Array.from(Array(STAGE_HEIGHT), ()=>
-        new Array(STAGE_WIDTH).fill([0, 'clear'])
-    )
-
